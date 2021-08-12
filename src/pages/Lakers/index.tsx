af209e775d6842de3f01535634547bc0b5f91935
saisai_jia <jiasai37501@163.com>
2021-08-13 00:09:15 +0800

    fix:修复lakers页面bug

--- src/pages/Lakers/index.tsx
+++ src/pages/Lakers/index.tsx
@@ -4,67 +4,67 @@ import { connect } from 'react-redux'
 import CompireRouter from '../../router/CompireRouter';
 
 const mapState = (state: any) => {
-    return {
-        state
-    }
+  return {
+    state
+  }
 }
 
 const mapDispatch = (dispatch: any) => {
-    return {}
+  return {}
 }
 
 const connects: Function = connect;
 
 @connects(mapState, mapDispatch)
-class Lakers extends React.Component {
-    constructor(props: any) {
-        super(props);
-        this.state = {};
-        // let { computedMatch, history, routes, match } = this.props;
-        // console.log("=====", this.props);
-    }
+class Lakers extends React.Component<any, any> {
+  constructor(props: any) {
+    super(props);
+    this.state = {};
+    // let { computedMatch, history, routes, match } = this.props;
+    // console.log("=====", this.props);
+  }
 
-    handleJump(history: any) {
-        history.push({
-            pathname: "/heat",
-            state: { name: "sai" },
-            params: { name: "liuDi" },
-            size: { size: "36" } // 可以随意传参
-        })
-    }
+  handleJump(history: any) {
+    history.push({
+      pathname: "/heat",
+      state: { name: "sai" },
+      params: { name: "liuDi" },
+      size: { size: "36" } // 可以随意传参
+    })
+  }
 
-    render() {
-        console.log("=====", this.props);
+  render() {
+    console.log("=====", this.props);
 
-        let { history, routes, match, state }: any = this.props;
+    let { history, routes, match, state }: any = this.props;
 
-        return (
+    return (
 
-            <Fragment>
-                <section>{state.Goods[0]}</section>
-                <ul>
-                    <li>
-                        <Link to={match.url + "/davis"}>戴维斯</Link>
-                    </li>
-                    <li>
-                        <Link to={"/lakers/kuzma"}>库兹马</Link>
-                    </li>
-                    <li>
-                        <Link to={match.url + "/lebron"}>詹姆斯</Link>
-                    </li>
-                </ul>
+      <Fragment>
+        <section>{state.Goods[0]}</section>
+        <ul>
+          <li>
+            <Link to={match.url + "/davis"}>戴维斯</Link>
+          </li>
+          <li>
+            <Link to={"/lakers/kuzma"}>库兹马</Link>
+          </li>
+          <li>
+            <Link to={match.url + "/lebron"}>詹姆斯</Link>
+          </li>
+        </ul>
 
-                {/* <CompireRouter routes={router[1].children}></CompireRouter> */}
-                <CompireRouter routes={routes}></CompireRouter>
+        {/* <CompireRouter routes={router[1].children}></CompireRouter> */}
+        <CompireRouter routes={routes}></CompireRouter>
 
 
-                <section>
-                    <button onClick={() => this.handleJump(history)}>热火队</button>
-                </section>
+        <section>
+          <button onClick={() => this.handleJump(history)}>热火队</button>
+        </section>
 
-            </Fragment>
-        )
-    }
+      </Fragment>
+    )
+  }
 }
 
 export default Lakers;
