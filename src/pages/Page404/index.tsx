9baee1865e8ad4c07ec977fbf0e9980930cbac53
saisai_jia <jiasai37501@163.com>
2021-08-15 23:40:28 +0800

    add: 404页面样式修改

--- src/pages/Page404/index.tsx
+++ src/pages/Page404/index.tsx
@@ -1,9 +1,19 @@
-import React, { Fragment } from "react";
+import React from "react";
+import './index.css'
 
 export default function Page404() {
-    return (
-        <Fragment>
-            <section>page404.......路径找不到了</section>
-        </Fragment>
-    )
+  return (
+    <div className='bg__box'>
+      <div className='bg__container'>
+        <div className='whiteSpace'></div>
+        <h2>抱歉页面无法访问</h2>
+        <br />
+        <h3>page404</h3>
+        <br />
+        <div>
+          <p>页面地址可能已经失效</p>
+        </div>
+      </div>
+    </div>
+  )
 }
