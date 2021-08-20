ba204dbcfcb01efe1de59c25b8e5a698044bd607
贾赛 <jiasai@aegis-data.cn>
2021-08-20 16:52:05 +0800

    add:page404页面修改

--- src/pages/Page404/index.tsx
+++ src/pages/Page404/index.tsx
@@ -1,17 +1,28 @@
 import React from "react";
 import './index.css'
+import notFound from './index.module.scss'
 
 export default function Page404() {
   return (
-    <div className='bg__box'>
-      <div className='bg__container'>
-        <div className='whiteSpace'></div>
-        <h1>抱歉页面无法访问</h1>
-        <br />
-        <h2>page404</h2>
-        <br />
-        <div>
-          <p>页面地址可能已经失效</p>
+    <div className={notFound.bg__box}>
+      <div className={notFound.bg__container}>
+        <div className={notFound.head404}></div>
+        <div className={notFound.txtbg404}>
+          <div className={notFound.txtbox}>
+            <p>对不起，您请求的页面不存在、或已被删除、或暂时不可用</p>
+            <p className={notFound.paddingbox}>请点击以下链接继续浏览网页</p>
+            <p>
+              》
+              <span>返回上一个页面</span>
+              <br />
+            </p>
+            <p>
+              》
+              <span>
+                返回网站首页
+              </span>
+            </p>
+          </div>
         </div>
       </div>
     </div>
