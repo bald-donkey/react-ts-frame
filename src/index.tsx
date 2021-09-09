965d3ceb21a98c7711faa29465fa074042f64b53
贾赛 <jiasai@aegis-data.cn>
2021-09-09 14:59:31 +0800

    add：首次渲染加载本地页面

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import store from './store'
+import FirstScreen from './pages/FirstScreen/index'
 import './assets/css/index.css';
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       <App />
+//     </Provider>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
+function listen() {
+  if (document.readyState === 'complete') { // 资源加载完成
+    ReactDOM.render(
+      <Provider store={store}>
+        <App />
+      </Provider>,
+      document.getElementById('root')
+    )
+  } else { // 资源加载中
+    ReactDOM.render(
+      <Provider store={store}>
+        <Router>
+          <Route path="/" component={FirstScreen} />
+        </Router>
+      </Provider>,
+      document.getElementById('root')
+    )
+  }
+}
+
+document.onreadystatechange = listen
