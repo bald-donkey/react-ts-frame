93bd28d2e4eb5e415d8e041c199af0937d1aa59b
贾赛 <jiasai@aegis-data.cn>
2021-09-27 09:45:33 +0800

    清空仓库

--- src/api/index.js
+++
@@ -1,8 +0,0 @@
-import axios from '../service/request'
-
-// export const getProduct = () => fetch('/api/data/index.json').then(body => body.json())
-
-export const getProduct = () => axios({
-  url: '/data/index.json',
-  method: 'get'
-})
--- src/store/reducers/Users.js
+++
@@ -1,6 +0,0 @@
-let defaultState = [1, 2, 3]
-
-
-export default function Users (state = defaultState, action) {
-  return [...state]
-}
